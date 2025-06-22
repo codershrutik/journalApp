b8ce12a54a15972c65886bff86260ab567162190
ShrutiKhandelwal <shrutikhandelwal@fico.com>
2025-06-22 18:03:34 +0530

    Initial commit

---
+++ src/main/java/com/example/journalapp/controller/HealthCheckController.java
@@ -0,0 +1,5 @@
+package com.example.journalapp.controller;
+
+public class HealthCheckController {
+
+}
---
+++ src/test/java/com/example/journalapp/JournalAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.journalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
